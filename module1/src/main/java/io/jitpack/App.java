dd03b879400285760c8c6163b2e45eb66e0a8663
tom <tom@example.com>
2019-01-05 17:49:03 +0000

    change in class

---
+++ module1/src/main/java/io/jitpack/App.java
@@ -0,0 +1,16 @@
+package io.jitpack;
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static final String GREETING = "Hello World!";
+    
+    public static void main( String[] args )
+    {
+        System.out.println(GREETING);
+    }
+}
